b9de28decfd450fe66ab6101986007b134a3d36e
gespel <heimbrodt@uni-potsdam.de>
2024-12-14 13:36:14 +0100

    added rotary

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -9,6 +9,18 @@ OscilloscopeAudioProcessorEditor::OscilloscopeAudioProcessorEditor (Oscilloscope
 
     setSize (512, 406);
     startTimer(20.0);
+
+    gain.setSliderStyle(juce::Slider::SliderStyle::Rotary);
+    gain.setRange(0.0, 100.0, 0.1);
+    gain.setTextBoxStyle(Slider::TextEntryBoxPosition::NoTextBox, true, 0, 0);
+    gain.addListener(this);
+    addAndMakeVisible(gain);
+
+    windowSize.setSliderStyle(juce::Slider::SliderStyle::Rotary);
+    windowSize.setRange(0.0, 3000.0, 0.1);
+    windowSize.setTextBoxStyle(Slider::TextEntryBoxPosition::NoTextBox, true, 0, 0);
+    windowSize.addListener(this);
+    addAndMakeVisible(windowSize);
 }
 
 OscilloscopeAudioProcessorEditor::~OscilloscopeAudioProcessorEditor()
@@ -24,7 +36,7 @@ void OscilloscopeAudioProcessorEditor::paint (juce::Graphics& g)
     g.setColour (juce::Colour(0xaa, 0x76, 0xb4));
     float* data = audioProcessor.getWindow();
     for(int i = 0; i < 474; i += 1) {
-        g.drawLine(i + 19, (data[i + sample_increase]*800) + 185, i + 20, (data[i + sample_increase + sample_increase]*800) + 185, 3);
+        g.drawLine(i + 19, (data[i + sample_increase]*gain_value) + 185, i + 20, (data[i + sample_increase + sample_increase]*gain_value) + 185, 3);
     }
     g.setFont (juce::FontOptions (11.0f));
     g.drawText("Made by Phi Labs", juce::Rectangle(11, 308, 100, 15), juce::Justification::centred);
@@ -32,5 +44,10 @@ void OscilloscopeAudioProcessorEditor::paint (juce::Graphics& g)
 
 void OscilloscopeAudioProcessorEditor::resized()
 {
+    gain.setBounds(32, 220, 67, 285);
+    //requencyWindow.setBounds();
+}
 
+void OscilloscopeAudioProcessorEditor::sliderValueChanged(juce::Slider* slider) {
+    gain_value = gain.getValue();
 }
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -7,7 +7,7 @@
 //==============================================================================
 /**
 */
-class OscilloscopeAudioProcessorEditor  : public juce::AudioProcessorEditor, private juce::Timer
+class OscilloscopeAudioProcessorEditor  : public juce::AudioProcessorEditor, private juce::Timer, public juce::Slider::Listener
 {
 public:
     OscilloscopeAudioProcessorEditor (OscilloscopeAudioProcessor&);
@@ -16,15 +16,19 @@ public:
     //==============================================================================
     void paint (juce::Graphics&) override;
     void resized() override;
+    juce::Slider gain;
+    juce::Slider windowSize;
 
 private:
     int sample_increase = 1;
+    double gain_value = 1;
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     OscilloscopeAudioProcessor& audioProcessor;
     void timerCallback() final {
         repaint();
     }
+    void sliderValueChanged(juce::Slider* slider) override;
     Image background = ImageCache::getFromMemory(BinaryData::oscilloscope_bg_png, BinaryData::oscilloscope_bg_pngSize);
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (OscilloscopeAudioProcessorEditor)
 };
