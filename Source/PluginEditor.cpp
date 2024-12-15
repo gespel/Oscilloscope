9093afd98e034c1d3b11915987fda53fa056234a
gespel <heimbrodt@uni-potsdam.de>
2024-12-15 12:42:47 +0100

    added window size knob

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -11,13 +11,13 @@ OscilloscopeAudioProcessorEditor::OscilloscopeAudioProcessorEditor (Oscilloscope
     startTimer(20.0);
 
     gain.setSliderStyle(juce::Slider::SliderStyle::Rotary);
-    gain.setRange(0.0, 100.0, 0.1);
+    gain.setRange(0.0, 1000.0, 0.1);
     gain.setTextBoxStyle(Slider::TextEntryBoxPosition::NoTextBox, true, 0, 0);
     gain.addListener(this);
     addAndMakeVisible(gain);
 
     windowSize.setSliderStyle(juce::Slider::SliderStyle::Rotary);
-    windowSize.setRange(0.0, 3000.0, 0.1);
+    windowSize.setRange(1, 4, 1);
     windowSize.setTextBoxStyle(Slider::TextEntryBoxPosition::NoTextBox, true, 0, 0);
     windowSize.addListener(this);
     addAndMakeVisible(windowSize);
@@ -36,7 +36,7 @@ void OscilloscopeAudioProcessorEditor::paint (juce::Graphics& g)
     g.setColour (juce::Colour(0xaa, 0x76, 0xb4));
     float* data = audioProcessor.getWindow();
     for(int i = 0; i < 474; i += 1) {
-        g.drawLine(i + 19, (data[i + sample_increase]*gain_value) + 185, i + 20, (data[i + sample_increase + sample_increase]*gain_value) + 185, 3);
+        g.drawLine(i + 19, (data[i + sample_increase] * gain_value) + 185, i + 20, (data[i + sample_increase + sample_increase] * gain_value) + 185, 3);
     }
     g.setFont (juce::FontOptions (11.0f));
     g.drawText("Made by Phi Labs", juce::Rectangle(11, 308, 100, 15), juce::Justification::centred);
@@ -44,10 +44,12 @@ void OscilloscopeAudioProcessorEditor::paint (juce::Graphics& g)
 
 void OscilloscopeAudioProcessorEditor::resized()
 {
-    gain.setBounds(32, 220, 67, 285);
+    gain.setBounds(42, 330, 67, 70);
+    windowSize.setBounds(202, 330, 67, 70);
     //requencyWindow.setBounds();
 }
 
 void OscilloscopeAudioProcessorEditor::sliderValueChanged(juce::Slider* slider) {
     gain_value = gain.getValue();
+    sample_increase = windowSize.getValue();
 }
